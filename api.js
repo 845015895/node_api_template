4f044a8e7ec57085e8431148c810982479e553ec
YZC <yzcformyself@163.com>
2018-03-11 10:11:14 +0800

    api模板“ ”

--- api.js
+++ api.js
@@ -1,98 +1,144 @@
-let urlLib = require('url');
-// let async = require('async');
+﻿let urlLib = require('url');
+let mysql = require('mysql');
+let async = require('async');
 let express = require('express');
 let bodyParser = require('body-parser');
-let http = require('http');
 
 let app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 //设置跨距
 app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, POST, DELETE, OPTIONS');
-    if (req.method === 'OPTIONS') {
-        res.send(200);
-    } else {
-        next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, POST, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
+
+let connection = mysql.createConnection({
+  host: 'xxxx',
+  user: 'root',
+  password: 'xxxx',
+  database: 'xxxx'
+});
+
+
+connection.connect();
+
+
+//注册接口
+app.post('/user/reg', function (req, res) {
+  //查询用户是否存在
+  let querySql = "";
+  let queryParams = [];
+  
+  // let obj = urlLib.parse(req.url, true);
+  // const url = obj.pathname;
+  // const POST = obj.query;
+
+  const POST = req.body;
+  async.waterfall([//处理数据库异步回掉
+    function selectUser(cb) {
+      queryParams = POST.user;
+      connection.query(querySql, queryParams, function (err, result) {
+        if (err) {
+          console.log('[SELECT ERROR] - ', err.message);
+          return;
+        }
+        userCount = result.length;
+        cb(null, userCount);
+      });
+    },
+    function addUser(userCount, cb) {
+      if (userCount) {
+        res.json(reg_err1);
+      } else {
+        addParams = [null,POST.user, POST.pass];
+        connection.query(addSql, addParams, function (err, result) {
+          if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+          }
+          cb(null, result);
+          res.json(reg_finish);
+        });
+      }
+
+    }]
+  );
+});
+
+
+//post
+app.post('/user/log', function (req, res) {
+  // 输出 JSON 格式
+  //  response.end(JSON.stringify(data));
+  // let obj = urlLib.parse(req.url, true);
+  // const POST = obj.query;
+  let querySql = "";
+  const POST = req.body;
+  let queryParams = [];
+  // let queryParams = POST.user;
+  connection.query(querySql, queryParams, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
     }
+      res.json(result);
+  });
 });
 
 
-// 查询排行榜
-app.get('/rank', function (req, res) {
-    let obj = urlLib.parse(req.url, true);
-    const GET = obj.query;
-
-    let rankId = GET.rankid;
-    let mainData = "";
-
-    http.get(`http://m.kugou.com/rank/info/?rankid=${rankId}&page=1&json=true`,function (req1,res1) {
-        req1.on('data',function(data){
-            mainData += data;
-        });
-        req1.on('end',function(){
-            res.send(mainData);
-        });
-        req1.on('error',function(){
-            res.send("请求失败");
-        });
-    });
-
+//get
+app.get('/user/list',function (req, res) {
+  let obj = urlLib.parse(req.url, true);
+  let queryAllSql = "";
+  let queryParams = [];
+  const GET = obj.query;
+  //queryParams = [GET.user];
+    connection.query(queryAllSql,queryParams,function (err, result) {
+      if (err) {
+        console.log('[SELECT ERROR] - ', err.message);
+        return;
+      }
+      res.json(result);
+    })
 });
+//delete
+app.delete('/user/delete',function (req, res) {
+  let obj = urlLib.parse(req.url, true);
+  let deleteSql = "";
+  const DELETE = obj.query;
+  let deleteId = "";
+  // let deleteId = DELETE.id;
+  connection.query(deleteSql,deleteId,function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
+    }
+    if(result.affectedRows){
+      res.json(deleteMsg);
+    }
 
-// 搜索音乐
-app.get('/search', function (req, res) {
-    let obj = urlLib.parse(req.url, true);
-    const GET = obj.query;
-
-    let keyword =encodeURIComponent(GET.keyword) ;
-    let mainData = "";
-    http.get(`http://songsearch.kugou.com//song_search_v2?keyword=${keyword}&page=1&clientver=&platform=WebFilter`,function (req1,res1) {
-
-        req1.on('data',function(data){
-            mainData += data;
-        });
-        req1.on('end',function(){
-            res.send(mainData);
-        });
-        req1.on('error',function(){
-            res.send("请求失败");
-        });
-    });
-
+  })
 });
-
-// 获取音乐
-app.get('/music', function (req, res) {
-    let obj = urlLib.parse(req.url, true);
-    const GET = obj.query;
-
-    let hash = GET.hash;
-    let mainData = "";
-    http.get(`http://www.kugou.com/yy/index.php?r=play/getdata&hash=${hash}`,function (req1,res1) {
-        req1.on('data',function(data){
-            mainData += data;
-        });
-        req1.on('end',function(){
-            res.send(mainData);
-        });
-        req1.on('error',function(){
-            res.send("请求失败");
-        });
-    });
-});
-
 
 
 
 //配置服务端口
-let server = app.listen(8888, function () {
-    let host = server.address().address;
-    let port = server.address().port;
-    console.log('listening at http://%s:%s', host, port);
+let server = app.listen(30000, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('listening at http://%s:%s', host, port);
 });
+
+
+
+
